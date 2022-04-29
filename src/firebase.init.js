e4d748bc5e868494559ff50943d294e0f3d0e698
ZeroBug <404.zerobug@gmail.com>
2022-04-29 14:42:57 +0600

    experience sec and service sec added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlb8pmq18AkjmnIAWY4ca21pIzVoNrdHs",
-  authDomain: "bicycle-inventory.firebaseapp.com",
-  projectId: "bicycle-inventory",
-  storageBucket: "bicycle-inventory.appspot.com",
-  messagingSenderId: "975793268628",
-  appId: "1:975793268628:web:9f0f90e28a2d8e9d8b9181"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
