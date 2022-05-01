55d92fca26536e723205c4b47b1048d000aa3adc
ZeroBug <404.zerobug@gmail.com>
2022-05-01 16:57:48 +0600

    loading spinner added

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Navigate, useLocation } from 'react-router-dom';
-import auth from '../../../firebase.init';
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../../firebase.init";
 
-const RequireAuth = ({children}) => {
-    const [user] = useAuthState(auth);
-    const location = useLocation();
+const RequireAuth = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
+  const location = useLocation();
 
-    if (!user) {
-        return <Navigate to="/login" state={{ from: location }} replace />;
-      }
+  if (loading) {
+    return (
+      <div style={{height: '81vh'}} className="w-100 d-flex justify-content-center   align-items-center">
+        <Spinner animation="border" variant="danger" />
+      </div>
+    );
+  }
 
-    return children;
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
 };
 
 export default RequireAuth;
